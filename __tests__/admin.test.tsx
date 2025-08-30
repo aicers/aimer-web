bc5febfe2f7fdf1cd5c01f43dd674d33de470c2f
sehkone <sehkone@cluml.com>
2025-08-30 22:12:03 +0900

    Remove `biome-ignore` comments

--- __tests__/admin.test.tsx
+++ __tests__/admin.test.tsx
@@ -1,6 +1,4 @@
 import { render, screen } from "@testing-library/react";
-// biome-ignore lint/correctness/noUnusedImports: needed for JSX in Vitest
-import React from "react";
 import AdminAppPage from "../src/app/admin/page";
 import "@testing-library/jest-dom";
 
